8d07169ddc346bea9584ee91a95c20d51ef80788
Dart18-80 <diegoruiztrabanino13@gmail.com>
2023-04-08 12:37:07 -0600

    Add Event Consumers

--- SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,8 +1,11 @@
+using Confluent.Kafka;
+using CQRS.Core.Consumers;
 using Microsoft.EntityFrameworkCore;
 using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.Repositories;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,8 +21,12 @@ dataContext.Database.EnsureCreated();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>(); 
+builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
+builder.Services.AddScoped<IEventConsumer, EventConsumer>();
 
 builder.Services.AddControllers();
+builder.Services.AddHostedService<ConsumerHostedService>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
