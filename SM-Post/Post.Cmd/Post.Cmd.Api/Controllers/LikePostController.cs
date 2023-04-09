ac798cfaa4a79954d03977022789b04853776aab
Dart18-80 <diegoruiztrabanino13@gmail.com>
2023-04-08 21:09:41 -0600

    Add Like Post Controller and Edit EditMessageController

--- SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/LikePostController.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/LikePostController.cs
@@ -12,28 +12,27 @@ namespace Post.Cmd.Api.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-
-    public class EditMessageController : ControllerBase
+    public class LikePostController : ControllerBase
     {
-        private readonly ILogger<EditMessageController> _logger;
+        private readonly ILogger<LikePostController> _logger;
         private readonly ICommandDispacher _commandDispatcher;
-        public EditMessageController( ILogger<EditMessageController> logger, ICommandDispacher commandDispatcher )
+
+        public LikePostController( ILogger<LikePostController> logger, ICommandDispacher commandDispatcher )
         {
             _logger = logger;
             _commandDispatcher = commandDispatcher;
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult> EditMessageAsync (Guid id, EditMessageCommand command)
+        public async Task<ActionResult> LikePostAsync (Guid id)
         {
             try
             {
-                command.Id = id;
-                await _commandDispatcher.SendAsync(command);
+                await _commandDispatcher.SendAsync(new LikePostCommand { Id = id });
 
                 return Ok( new BaseResponse 
                 {
-                    Message = "Edit message request completed successfully!"
+                    Message = "Like post request completed successfully!"
                 });
             }
             catch(InvalidOperationException ex)
@@ -54,7 +53,7 @@ namespace Post.Cmd.Api.Controllers
             }
             catch (Exception ex)
             {
-                const string SAFE_ERROR_MESSAGE = "Error while processing request to edit the message of a post!";
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to like a post!";
                 _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
                 
                 return StatusCode( StatusCodes.Status500InternalServerError, new BaseResponse
