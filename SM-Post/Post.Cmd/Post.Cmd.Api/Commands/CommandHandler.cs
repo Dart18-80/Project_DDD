3aa0dd6eccb831a24fe811778665b8ab4d6bc3f8
Dart18-80 <diegoruiztrabanino13@gmail.com>
2023-04-09 13:18:33 -0600

    Create Functionality to restore the read Database

--- SM-Post/Post.Cmd/Post.Cmd.Api/Commands/CommandHandler.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/CommandHandler.cs
@@ -67,5 +67,10 @@ namespace Post.Cmd.Api.Commands
 
             await _eventSourcingHandler.SaveAsync(aggregate);
         }
+
+        public async Task HandleAsync(RestoreReadDbCommand command)
+        {
+            await _eventSourcingHandler.RepublishEventsAsync();
+        }
     }
 }
--- SM-Post/Post.Cmd/Post.Cmd.Api/Commands/ICommandHandler.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/ICommandHandler.cs
@@ -14,6 +14,6 @@ namespace Post.Cmd.Api.Commands
         Task HandleAsync(EditCommentCommand command);
         Task HandleAsync(RemoveCommentCommand command);
         Task HandleAsync(DeletePostCommand command);
-
+        Task HandleAsync(RestoreReadDbCommand command);
     }
 }
